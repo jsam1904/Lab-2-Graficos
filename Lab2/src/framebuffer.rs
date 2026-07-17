6d07d295244f5cc77c7b901f5031ff6f02959ea1
jsam1904 <javierse.alvarado@gmail.com>
2026-07-17 13:57:01 -0600

    feat/ Agregar estructura del FrameBuffer

---
+++ Lab2/src/framebuffer.rs
@@ -0,0 +1,33 @@
+use raylib::prelude::Color;
+
+pub struct FrameBuffer {
+    pub width: i32,
+    pub height: i32,
+    pub pixels: Vec<Color>,
+}
+
+impl FrameBuffer {
+    pub fn new(width: i32, height:i32, background_color: Color) -> Self{
+        FrameBuffer {
+            width,
+            height,
+            pixels: vec![background; (width * height) as usize],
+        }
+    }
+
+    pub fn point(&mut self, x: i32, y: i32, color: Color){
+        if x < 0 || y < 0 || x >= self.width || y >= self.height {
+            return;
+        }
+        let idx = (y * self.width + x) as usize;
+        self.pixels[idx] = color;
+    }
+
+    pub fn get_color(alive: bool) -> Color {
+        if alive {
+            Color::new(255, 255, 255, 255)
+        } else {
+            Color::new(15, 15, 25, 255)
+        }
+    }
+}
